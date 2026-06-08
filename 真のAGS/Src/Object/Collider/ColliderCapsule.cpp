86aefef56b8c727b6548967a705f34532a46a9cc
ASOST\2416030 <nomoto.aren.1126@gmail.com>
2026-06-08 12:55:20 +0900

    いったん

--- 真のAGS/Src/Object/Collider/ColliderCapsule.cpp
+++ 真のAGS/Src/Object/Collider/ColliderCapsule.cpp
@@ -192,32 +192,38 @@ void ColliderCapsule::DrawDebug(int color)
 	// ã‚Ì‹…‘Ì
 	VECTOR pos1 = GetPosTop();
 	DrawSphere3D(pos1, radius_, 5, color, color, false);
+
 	// ‰º‚Ì‹…‘Ì
 	VECTOR pos2 = GetPosDown();
 	DrawSphere3D(pos2, radius_, 5, color, color, false);
 	VECTOR dir;
 	VECTOR s;
 	VECTOR e;
+
 	// ‹…‘Ì‚ğŒq‚®ü(X+)
 	dir = follow_->GetRight();
 	s = VAdd(pos1, VScale(dir, radius_));
 	e = VAdd(pos2, VScale(dir, radius_));
 	DrawLine3D(s, e, color);
+
 	// ‹…‘Ì‚ğŒq‚®ü(X-)
 	dir = follow_->GetLeft();
 	s = VAdd(pos1, VScale(dir, radius_));
 	e = VAdd(pos2, VScale(dir, radius_));
 	DrawLine3D(s, e, color);
+
 	// ‹…‘Ì‚ğŒq‚®ü(Z+)
 	dir = follow_->GetForward();
 	s = VAdd(pos1, VScale(dir, radius_));
 	e = VAdd(pos2, VScale(dir, radius_));
 	DrawLine3D(s, e, color);
+
 	// ‹…‘Ì‚ğŒq‚®ü(Z-)
 	dir = follow_->GetBack();
 	s = VAdd(pos1, VScale(dir, radius_));
 	e = VAdd(pos2, VScale(dir, radius_));
 	DrawLine3D(s, e, color);
+
 	// ƒJƒvƒZƒ‹‚Ì’†S
 	DrawSphere3D(GetCenter(), 5.0f, 10, color, color, true);
 }
